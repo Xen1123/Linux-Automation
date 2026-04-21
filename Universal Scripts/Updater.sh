7944a29f618187060cd6eea15951911fe67be4b6
Eric Luce <eluce11232011@gmail.com>
2026-04-21 22:01:32 +0000

    Enhance updater script with silent update logging
    Redirect error output to log files for various package managers and update echo messages to indicate silent updates.

--- Universal Scripts/Updater.sh
+++ Universal Scripts/Updater.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 echo "This Script Updates Your Arch, Debian, Or Fedora System! Press Any Key To Continue!"
-    read -n 1 -s
+    read -r -n 1 -s
         clear && sudo -v
 
 if command -v pacman >/dev/null 2>&1; then
-    echo "Arch Linux Found!"
+    echo "Arch Linux Found! Now Updating Silently!"
         sleep 1.5
-    sudo pacman -Syyu --noconfirm
+    sudo pacman -Syyu --noconfirm >/dev/null 2>>$HOME/pacman_error.log1 || { clear; echo "You Have No Internet Or You Lack Sudo!"; exit 1; }
 clear
 
 elif command -v apt >/dev/null 2>&1; then
-    echo "Debian Discovered!"
+    echo "Debian Found! Now Updating Silently!"
         sleep 1.5
             if command -v nala >/dev/null 2>&1; then
-                sudo nala upgrade -y
+                sudo nala upgrade -y >/dev/null 2>>$HOME/apt_error.log || { clear; echo "You Have No Internet Or You Lack Sudo!"; exit 1; }
             fi
 
             if ! command -v nala >/dev/null 2>&1; then
-                sudo apt update -y && sudo apt upgrade -y
+                sudo apt update -y && sudo apt upgrade -y >/dev/null 2>>$HOME/apt_error.log || { clear; echo "You Have No Internet Or You Lack Sudo!"; exit 1; }
             fi
 clear
 
 elif command -v dnf >/dev/null 2>&1; then
-    echo "Fedora Found!"
+    echo "Fedora Found! Now Updating Silently!"
         sleep 1.5
-    sudo dnf update -y
+    sudo dnf update -y >/dev/null 2>>$HOME/dnf_error.log || { clear; echo "You Have No Internet Or You Lack Sudo!"; exit 1; }
 clear
 fi
 
@@ -48,20 +48,19 @@ do
                     case $opt in
                         "Yes")
                             if command -v apt >/dev/null 2>&1; then
-                                sudo apt install flatpak -y
-                            fi
-                            if command -v pacman >/dev/null 2>&1; then
-                                sudo pacman -S flatpak --noconfirm
-                            fi
-                            if command -v dnf >/dev/null 2>&1; then
-                                sudo dnf install flatpak -y
+                                sudo apt install flatpak -y >/dev/null 2>>$HOME/apt_error.log || { clear; echo "You Have No Internet Or You Lack Sudo!"; exit 1; }
+                            elif command -v pacman >/dev/null 2>&1; then
+                                sudo pacman -S flatpak --noconfirm >/dev/null 2>>$HOME/pacman_error.log || { clear; echo "You Have No Internet Or You Lack Sudo!"; exit 1; }
+                            elif command -v dnf >/dev/null 2>&1; then
+                                sudo dnf install flatpak -y >/dev/null 2>>$HOME/dnf_error.log || { clear; echo "You Have No Internet Or You Lack Sudo!"; exit 1; }
                             fi
                             flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
                             break
                         esac
                     done
             fi
-            flatpak update -y
+            echo "Updating Flatpak Applications Silently!"
+            flatpak update -y >/dev/null 2>>$HOME/flatpak_error.log
             exit
             ;;
         "No")
